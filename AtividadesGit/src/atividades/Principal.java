df3e5724f2e2a928f3e13d9551c9697332d18742
Gustavo Weschenfelder <gustavowes2018@gmail.com>
2026-08-31 18:52:12 -0300

    feat: cria projeto AtividadesGit com a classe Principal

---
+++ AtividadesGit/src/atividades/Principal.java
@@ -0,0 +1,11 @@
+package atividades;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        System.out.println("Aluno: Gustavo Weschenfelder");
+        System.out.println("Curso: Programacao Java");
+        System.out.println("Atividades de Git e GitHub");
+    }
+    
+}
